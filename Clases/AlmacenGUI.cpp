6992922d4bf4812c1062dd533605fca31276e6db
Juanma Rico <juanmard@gmail.com>
2016-03-12 00:54:07 +0100

    AlmacenGUI: Se elimina la dependencia de la plantilla para el procedimiento 'list_getter'.

--- Clases/AlmacenGUI.cpp
+++ Clases/AlmacenGUI.cpp
@@ -14,11 +14,14 @@ DIALOG AlmacenGUI::dlg_plantilla[] =
 {
      /* (proc)          (x)  (y)  (w)  (h)  (fg) (bg) (key) (flags) (d1) (d2) (dp)                                 (dp2) (dp3) */
    { d_box_proc   ,        584, 176, 208, 248, 67, 243,   0,    0,      0,   0,   NULL,                             NULL, NULL },
-   { AlmacenGUI::callback,          592, 200, 192, 56,  67, 243,   0,    0,      0,   0,   (void *) AlmacenGUI::listGetter, NULL, NULL },
-   { d_text_proc,          592, 184, 128, 8,   67, 243,   0,    0,      0,   0,   (void *)"Almacén : ",             NULL, NULL },
+   { AlmacenGUI::callback, 592, 200, 192, 56,  67, 243,   0,    0,      0,   0,   (void *) list_getter,             NULL, NULL },
+   { d_text_proc,          592, 184, 128, 8,   67, 243,   0,    0,      0,   0,   (void *)"Nombre del fichero",     NULL, NULL },
    { d_bitmap_proc,        592, 264, 192, 152, 67, 243,   0,    0,      0,   0,   NULL,                             NULL, NULL },
    { NULL,                   0,   0,   0,   0,  0,   0,   0,    0,      0,   0,   NULL,                             NULL, NULL }
 };
+
+/* Variable estática que referencia al almacen activo. */
+Almacen * AlmacenGUI::almacen_activo = NULL;
 
 /**
  * \brief   Constructor de la GUI para el almacén dado por parámetro.
@@ -26,6 +29,7 @@ DIALOG AlmacenGUI::dlg_plantilla[] =
  */
 AlmacenGUI::AlmacenGUI (Almacen &almacen)
 {
+  setAlmacen (almacen);
   dlg_plantilla[1].dp3 = (void *) &almacen;
   dlg_plantilla[2].dp = const_cast<char *>((new string("Almacén: \"" + almacen.getNombre() + "\""))->c_str());
   dlg_plantilla[3].dp = screen;
@@ -40,9 +44,16 @@ AlmacenGUI::AlmacenGUI (Almacen &almacen)
 
 /**
  * \brief   Constructor por omisión.
+ * \details Mostramos simplemente la plantilla y la guardamos en 'dlg'.
  */
 AlmacenGUI::AlmacenGUI ()
 {
+  dlg_plantilla[3].dp = screen;
+  dlg.clear ();
+  dlg.push_back (dlg_plantilla[0]);
+  dlg.push_back (dlg_plantilla[1]);
+  dlg.push_back (dlg_plantilla[2]);
+  dlg.push_back (dlg_plantilla[3]);
 };
 
 /**
@@ -60,4 +71,12 @@ AlmacenGUI::~AlmacenGUI ()
 vector<DIALOG> &  AlmacenGUI::getGUI ()
 {
   return dlg;
+}
+
+/**
+ * \brief   Cambiamos el objeto 'almacen' a visualizar en la GUI.
+ */
+void  AlmacenGUI::setAlmacen (Almacen &almacen)
+{
+  almacen_activo = &almacen;
 }
--- Clases/AlmacenGUI.h
+++ Clases/AlmacenGUI.h
@@ -37,46 +37,44 @@ class AlmacenGUI
                       AlmacenGUI     (Almacen &almacen);
                       AlmacenGUI     ();
                       ~AlmacenGUI    ();
+    void              setAlmacen     (Almacen &almacen);
     vector<DIALOG> &  getGUI         ();
 
   private:
-    vector<DIALOG>  dlg;
+    vector<DIALOG>    dlg;
 
   public:
+    static Almacen *almacen_activo;
     static DIALOG dlg_plantilla[];
     /**
      * \brief   Método para rellenar el diálogo de la lista.
      * \todo    Eliminar la dependencia con la plantilla estática.
      */
-    static char *  listGetter (int index, int *list_size)
+    static char *  list_getter (int index, int *list_size)
     {
       // NOTA: Para no referirnos a la plantilla estática (ha eliminar) podemos usar la variable
       //       global de Allegro: 'active_dialog' y probar si funciona.
-      if (dlg_plantilla[1].dp3)
+      if (almacen_activo)
       {
-        // Creamos una referencia temporal al objeto.
-        Almacen &obj = *(static_cast<Almacen *>(dlg_plantilla[1].dp3));
-        
         // Si 'index' es negativo debe devolver NULL e indicar el tamaño de la lista.
         // Si 'index' es cero o positivo debe devolver la cadena del nombre del actor.
         if (index < 0)
         {
-          *list_size = obj.getSize ();
+          *list_size = almacen_activo->getSize ();
           return NULL;
         }
         else
         {
-          // Damos un poco de vueltas aquí...
-          // Primero obtenemos el puntero del bitmap mediante un índice y con el puntero buscamos el nombre.
-          // Mejor generar una nueva función en la clase Almacen que nos genere el nombre mediante el índice.
-//          return const_cast<char *>(obj.getName(obj.getBitmap (index)).c_str());
-          return const_cast<char *>(obj.getNombre (index).c_str());
+          return const_cast<char *>(almacen_activo->getNombre (index).c_str());
         }
       }
     };
 
     /**
      * \brief   Callback de la lista que es la que controla el resto del diálogo.
+     * \details Aunque es una función estática de la clase, esta funcionaría para
+     *          todos los objetos siempre que guardemos en d[0].dp3 el almacén al
+     *          que hace referencia.
      */
     static int callback (int msg, DIALOG *d, int c)
     {
@@ -93,6 +91,10 @@ class AlmacenGUI
         // Se procesan los mensajes.
         switch (msg)
         {
+          case MSG_START:
+              almacen_activo = (Almacen *) d[0].dp3;
+              break;
+
           case MSG_IDLE:
              // Si cambia el índice es que debemos actualizar el bitmap
               // y mandarle un mensaje para que se refresque.
@@ -104,13 +106,22 @@ class AlmacenGUI
                 d[2].dp = fondo;
                 object_message(&d[2], MSG_DRAW, 0);
                 d[2].dp = obj.getBitmap (d[0].d1);
+                destroy_bitmap (fondo);
                 indice_ant = d[0].d1;
                 return object_message(&d[2], MSG_DRAW, 0);
+//                return D_REDRAWME;
               }
               break;
 
-          case MSG_GOTMOUSE:
-          case MSG_LOSTMOUSE:
+          case MSG_GOTFOCUS:
+              // Si tenemos el foco, puesto que este callback va en la lista, actualizamos
+              // el almacén activo que lo tenemos guardado en 'dp3'.
+              almacen_activo = (Almacen *) d[0].dp3;
+              d[0].bg = 250;
+              break;
+
+          case MSG_LOSTFOCUS:
+              d[0].bg = 243;
               break;
         }
       }
