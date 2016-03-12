ea8d1db68dc6f9ce647ab21f84dece0be05d44ba
Juanma Rico <juanmard@gmail.com>
2016-03-12 03:01:06 +0100

    Se vuelve a generar las líneas de código perdido del loro.

--- Clases/Loro.cpp
+++ Clases/Loro.cpp
@@ -7,9 +7,6 @@
 ///
 
 #include "Loro.h"
-#include "ControllableActor.h"
-#include "Almacen.h"
-#include "Sprite.h"
 
 Loro::Loro ():
 ControllableActor ()
@@ -19,11 +16,27 @@ ControllableActor ()
 Loro::Loro (Almacen &almacen)
 {
     // ¡Cuidado! Esto falla si en el almacén no existe el bitmap que se pide.
-    Sprite *skin = new Sprite(this);    
-    skin->add_frame(almacen.getBitmap("sprite_042"), 0, 13, 10);
-    skin->add_frame(almacen.getBitmap("sprite_043"), 0,  0, 10);
-    skin->add_frame(almacen.getBitmap("sprite_044"), 0,  3, 10);
-    set_actor_graphic (skin);
+    // Animación de la derecha.
+    izquierda = new Sprite(this);
+    izquierda->add_frame(almacen.getBitmap("sprite_042"), 0, 13, 10);
+    izquierda->add_frame(almacen.getBitmap("sprite_043"), 0,  0, 10);
+    izquierda->add_frame(almacen.getBitmap("sprite_044"), 0,  3, 10);
+    izquierda->setMirror(true);
+
+    // Animación de la izquierda.
+    derecha = new Sprite(this);
+    derecha->add_frame(almacen.getBitmap("sprite_042"), 0, 13, 10);
+    derecha->add_frame(almacen.getBitmap("sprite_043"), 0,  0, 10);
+    derecha->add_frame(almacen.getBitmap("sprite_044"), 0,  3, 10);
+
+    // Animación de giro.
+    giro = new Sprite(this);
+    giro->add_frame(almacen.getBitmap("pre2_338"), 0, 13, 10);
+    giro->add_frame(almacen.getBitmap("pre2_338"), 0,  0, 10);
+    giro->add_frame(almacen.getBitmap("pre2_338"), 0,  3, 10);
+
+    // Animación de inicio.
+    set_actor_graphic (derecha);
 
     setCodigo (Nombres::herny);
     set_x(SCREEN_W/2);
@@ -41,17 +54,35 @@ void Loro::do_action (ControllableActor::action_t act, int magnitude)
         case UP:    y-=1; break;
         case LEFT:
           x-=1;
-          static_cast<Sprite *>(agraph)->setMirror (true);
+          /// @todo Habría que comparar con los estados actuales del actor, pero para pruebas
+          ///       se comparará los punteros de gráficos.
+          ///       Debería ser algo como...
+          ///       @code
+          ///           typedef enum {VOLANDO_IZQ, VOLAMDO_DER, GIRANDO} estado_t;
+          ///           if (estado_actual == Loro::VOLANDO_IZQ) {};
+          ///       @endcode
+          ///
+          if (this->get_actor_graphic() == derecha)
+          {
+            this->set_actor_graphic(giro);
+          }
+          else
+          {
+            this->set_actor_graphic(izquierda);
+          }
           break;
+
         case RIGHT:
           x+=1;
-          static_cast<Sprite *>(agraph)->setMirror (false);
+          this->set_actor_graphic(derecha);
           break;
-        case JUMP:  y-=4; break;
+
+        case JUMP:  y-=4;
+            break;
     }
 };
 
-void Loro::getNombre (string &strNombre) const
+void Loro::getNombre (std::string &strNombre) const
 {
     strNombre = Nombres::Imprimir (nombre);
 };
--- Clases/Loro.h
+++ Clases/Loro.h
@@ -11,6 +11,7 @@
 
 #include "ControllableActor.h"
 #include "Almacen.h"
+#include "Sprite.h"
 
 /// @class Loro
 /// Loro que se mueve siguiendo una horizontal.
@@ -23,30 +24,40 @@
 class Loro : public ControllableActor
 {
 public:
-    // Definición de acciones del loro.
-    typedef enum {DOWN, UP, LEFT, RIGHT, JUMP} action_t;
+    /// @typedef Definición de acciones del loro.
+    typedef enum {
+                DOWN,   ///< El loro debe bajar.
+                UP,     ///< El loro debe subir.
+                LEFT,   ///< El loro debe moverse a la izquierda.
+                RIGHT,  ///< El loro debe moverse a la derecha.
+                JUMP    ///< El loro debe saltar.
+                } action_t;
 
 public:
     /// Constructor básico.
+    ///
     Loro ();
 
-    /// Constructor.
-    /// @param almacen Almacen con los gráficos para crear la animación del loro.
-    /// @todo Este parámetro de almacen no tiene mucho sentido en esta clase.
+    /// Constructor mediante almacén de gráficos.
+    /// @param almacen  Almacen con los gráficos para crear la animación del loro.
+    /// @todo Este parámetro de almacen no tiene mucho sentido en esta clase, debería ser
+    ///       más sencillo crear un actor del tipo Loro.
     ///
     Loro (Almacen &almacen);
 
+    /// Realiza una acción del loro.
+    /// @param act  Acción a realizar por el loro.
+    /// @param magnitude  Fuerza con la que se realiza la acción.
+    ///
     void do_action (ControllableActor::action_t act, int magnitude);
 
     /// Obtiene el nombre en forma de cadena de texto.
     /// @param strNombre Referencia a la cadena donde se guardará el nombre del loro.
     ///
-    void getNombre (string &strNombre) const;
-
-protected:
+    void getNombre (std::string &strNombre) const;
 
 private:
-
+    Sprite *derecha, *izquierda, *giro;     ///< Animaciones para el actor Loro.
 };
 
 #endif
