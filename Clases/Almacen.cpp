7e756855bcaa216483492a192b694570fbed3541
Juanma Rico <juanmard@gmail.com>
2016-03-12 02:23:39 +0100

    Se redocumenta la clase Almacen.

--- Clases/Almacen.cpp
+++ Clases/Almacen.cpp
@@ -1,228 +1,143 @@
-/**
- *  \file     Almacen.cpp
- *  \brief    Fichero con el código de implementación de la clase Almacen.
- *
- *  \author   Juan Manuel Rico
- *  \date     Diciembre 2010
- *  \version  1.00
- *
- */
+///
+/// @file Almacen.cpp
+/// @brief Fichero con el código de implementación de la clase Almacen.
+/// @author Juan Manuel Rico
+/// @date Diciembre 2010
+/// @version
+///      - 1.0.0 Diciembre 2010
+///      - 1.0.1 Marzo 2015
+///
+
 #include "Almacen.h"
 #include "AlmacenGUI.h"
 
-/**
- * \brief   Construye el almacén dando el nombre del fichero DAT a cargar.
- * \details Intenta leer el fichero, si consigue abrirlo genera tablas de los distintos tipos de
- *          recursos y sus nombres dados en el fichero.
- *          Si no consigue abrirlo, envía un mensaje de error por consola.
- *          Se crea también una GUI para poder mostrar todos los recursos.
- *
- * \param   paramNombreFichero  Cadena con el nombre del fichero de tipo DAT.
- *
- * \todo    Por hacer:
- *          - Cuando se produzca el error en la lectura del fichero lazar una excepción para ser capturada.
- *          - Eliminar la creación de la GUI en el constructor. Ponerla en un método aparte.
- */
-Almacen::Almacen (string paramNombreFichero)
+Almacen::Almacen (std::string paramNombreFichero)
 {
-  // Se intenta cargar el fichero de datos en memoria.
-  fichero = load_datafile (paramNombreFichero.c_str ());
-  if (fichero)
-  {
-    // Se pudo cargar el fichero de datos en memoria. Guardamos el nombre del fichero.
-    nombreFichero = paramNombreFichero;
-
-    // Recorremos los datos y emparejamos nombres con punteros de los distintos recursos.
-    string nombre;
-    for (int i=0; fichero[i].type != DAT_END; i++)
+    // Se intenta cargar el fichero de datos en memoria.
+    fichero = load_datafile (paramNombreFichero.c_str ());
+    if (fichero)
     {
-      nombre = get_datafile_property(&fichero[i], DAT_ID('N','A','M','E'));
-      switch (fichero[i].type)
-      {
-        case DAT_BITMAP:
-              bitmaps[nombre] = (BITMAP *) fichero[i].dat;
-              // cout << "Bitmap >> " << nombre << "," << bitmaps[nombre] << endl;
-              break;
-        case DAT_PALETTE:
-              paletas[nombre] = (RGB *) fichero[i].dat;
-              // cout << "Paleta >> " << nombre << "," << paletas[nombre] << endl;
-              break;
-        case DAT_SAMPLE:
-              sonidos[nombre] = (SAMPLE *) fichero[i].dat;
-              // cout << "Sonido >> " << nombre << "," << sonidos[nombre] << endl;
-              break;
-        default:
-              // cout << "DAT desconocido >> " << nombre << "," << fichero[i].dat << endl;
-              break;
-      }
-    }
-  }
-  else
-  {
-    // No se encontró el fichero. Borramos la cadena del nombre. Avisamos en consola.
-    nombreFichero.clear ();
-    cout << "ERROR: No se pudo acceder al fichero de datos: \"" << paramNombreFichero << "\"" << endl \
-         << "Compruebe que se encuentra en el mismo directorio que el juego." << endl;
-  }
+        // Se pudo cargar el fichero de datos en memoria. Guardamos el nombre del fichero.
+        nombreFichero = paramNombreFichero;
 
-  // Se crea una GUI para esta clase.
-  gui = new AlmacenGUI (*this);
+        // Recorremos los datos y emparejamos nombres con punteros de los distintos recursos.
+        std::string nombre;
+        for (int i=0; fichero[i].type != DAT_END; i++)
+        {
+            nombre = get_datafile_property(&fichero[i], DAT_ID('N','A','M','E'));
+            switch (fichero[i].type)
+            {
+            case DAT_BITMAP:
+                bitmaps[nombre] = (BITMAP*) fichero[i].dat;
+                // cout << "Bitmap >> " << nombre << "," << bitmaps[nombre] << endl;
+                break;
+
+            case DAT_PALETTE:
+                paletas[nombre] = (RGB*) fichero[i].dat;
+                // cout << "Paleta >> " << nombre << "," << paletas[nombre] << endl;
+                break;
+
+            case DAT_SAMPLE:
+                sonidos[nombre] = (SAMPLE*) fichero[i].dat;
+                // cout << "Sonido >> " << nombre << "," << sonidos[nombre] << endl;
+                break;
+
+            default:
+                // cout << "DAT desconocido >> " << nombre << "," << fichero[i].dat << endl;
+                break;
+            }
+        }
+    }
+    else
+    {
+        // No se encontró el fichero. Borramos la cadena del nombre. Avisamos en consola.
+        nombreFichero.clear ();
+        cout << "ERROR: No se pudo acceder al fichero de datos: \"" << paramNombreFichero << "\"" << endl \
+             << "Compruebe que se encuentra en el mismo directorio que el juego." << endl;
+    }
+
+    // Se crea una GUI para esta clase.
+    gui = new AlmacenGUI (*this);
 };
 
-/**
- * \brief   Constructor por omisión.
- * \details Crea un almacén vacío y sin GUI.
- */
 Almacen::Almacen ():
 nombreFichero (NULL),
 gui (NULL)
 {
 };
 
-/**
- * \brief   Destructor por omisión.
- * \todo    Liberar los recursos correctamente.
- */
 Almacen::~Almacen ()
 {
-  // Liberar los recursos del fichero.
-  // Liberar los recursos 'map'.
+    // Liberar los recursos del fichero.
+    // Liberar los recursos 'map'.
 };
 
-/**
- * \brief   Devuelve un 'BITMAP allegro' como recurso dando el nombre del bitmap.
- * \details Este método se simplifica gracias a la carga previa en la clase 'map'. Se usa como índice
- *          de la tabla 'map' el nombre obtenido del fichero DAT.
- * \param   nombreBitmap  Nombre identificativo del bitmap.
- * \return  Puntero a la zona de memoria donde se encuentra guardado el BITMAP. 
- */
-BITMAP *  Almacen::getBitmap (string nombreBitmap)
+BITMAP* Almacen::getBitmap (std::string nombreBitmap)
 {
-  return bitmaps[nombreBitmap];
+    return bitmaps[nombreBitmap];
 };
 
-/**
- * \brief   Devuelve un 'BITMAP allegro' como recurso dando el índice.
- * \details Este índice es el número identificativo indicado en el fichero
- *          '.H' generado por Grabber.
- * \param   indice  Índice del recurso que se busca.
- * \return  Puntero a la zona de memoria donde se encuentra guardado el BITMAP.
- *          Si el tipo de recurso apuntado por el índice no es un BITMAP devuelve NULL.
- *
- * \todo    Comprobar que el índice se encuentra dentro del máximo de recursos.
- */
-BITMAP *  Almacen::getBitmap (int indice)
+BITMAP* Almacen::getBitmap (int indice)
 {
-  if (fichero[indice].type == DAT_BITMAP)
-  {
-    return (BITMAP *) fichero[indice].dat;
-  }
-  else
-  {
-    return NULL;
-  }
-};
-
-/**
- * \brief   Devuelve el nombre de un recurso según el índice en el fichero.
- * \details El nombre que se devuelve es el que se encuentra asociado al índice en el fichero DAT.
- * \param   indice  Índice del recurso del que obtener el nombre.
- * \return  Cadena con el nombre del recurso, según el obtenido del fichero DAT.
- */
-string  Almacen::getNombre (int indice) const
-{
-  return get_datafile_property(&fichero[indice], DAT_ID('N','A','M','E'));
-};
-
- /**
- * \brief   Devuelve un puntero a un array de 256 estructuras RGB (Paleta de colores).
- * \details Este procedimiento se simplifica gracias a la carga previa en la clase 'map'. Se usa como
- *          índice el nombre de la paleta en el fichero DAT.
- * \param   nombrePaleta  Cadena con el nombre de la paleta que queremos obtener.
- * \return  Puntero a la zona de memoria de las 256 estructuras RGB que conforman la paleta de colores.
- */
-RGB *  Almacen::getPalette (string nombrePaleta)
-{
-  return paletas[nombrePaleta];
-};
-
-/**
- * \brief   Devuelve el nombre asociado al puntero en 'bitmaps'.
- * \details Recorre todo el 'map' y compara los punteros hasta encontrar el dado por parámetro.
- *          Procedimiento lento. Se utiliza para guardar el BITMAP como cadena, que sólo se usará
- *          en casos puntuales del juego.
- * \param   puntero   Puntero del bitmap del que queremos obtener su nombre.
- * \return  Cadena nueva de caracteres con el nombre asociado al puntero. Si no encuentra la
- *          asociación con el puntero devolverá una cadena vacía.
- * \todo    Por hacer:
- *          - En lugar de pasar como parámetro el puntero pasar una referencia constante al puntero
- *            y así evitar modificaciones accidentales del mismo.
- *          - Confirmar que se devuelve una cadena vacía si no se encuentra asociación con el puntero.
- *          - Usar una constante DEBUG para el precompilador en lugar de comentarlas.
- */
-string  Almacen::getName (BITMAP *puntero)
-{
-  string nombre;
-  map<string,BITMAP *>::iterator  it;
-
-  it = bitmaps.begin ();
-  while ( (it!=bitmaps.end()) && nombre.empty() )
-  {
-    // Para 'debug' mostramos el proceso de búsqueda.
-    // cout << it->first << " => " << it->second << endl;
-    if (it->second == puntero)
+    if (fichero[indice].type == DAT_BITMAP)
     {
-      nombre = it->first;
+        return (BITMAP*) fichero[indice].dat;
     }
-    it++;
-  }
-  // Para 'debug' mostramos los valores finales.
-  // cout << "Puntero de bitmap a buscar: " << puntero << endl \
-  //      << "Nombre asociado en 'map': " << nombre << endl;
-  return nombre;
+    else
+    {
+        return NULL;
+    }
 };
 
-/**
- * \brief   Pasa de un dialog estático a otro dinámico.
- * \todo    Confirmar que este método es usado y es necesario.
- */
-vector<DIALOG> &  Almacen::getDIALOG ()
+std::string Almacen::getNombre (int indice) const
 {
-  return gui->getGUI ();
+    return get_datafile_property(&fichero[indice], DAT_ID('N','A','M','E'));
 };
 
-/**
- * \brief   Obtiene el número total de recursos en el almacén.
- * \details Se obtienen los recursos guardados desde el fichero DAT al crear el almacén.
- *          Pueden existir recursos de distinto tipo a los guardados en el fichero y estos no se
- *          contarán.
- * \return  El número de recursos que se encuentran en el almacén.
- *
- * \todo    Añadir una variable en la clase que nos diga el número de recursos en el fichero que
- *          existen, pero que no se pudieron clasificar.
- */
-unsigned int  Almacen::getSize () const
+RGB* Almacen::getPalette (std::string nombrePaleta)
 {
-  return (bitmaps.size () + paletas.size () + sonidos.size ());
+    return paletas[nombrePaleta];
 };
 
-/**
- * \brief   Obtiene el nombre del fichero desde donde se han cargado los recursos del almacén.
- * \return  Nombre del fichero DAT desde donde se obtuvieron los recursos.
- */
-string  Almacen::getNombre () const
+std::string Almacen::getName (BITMAP* puntero)
 {
-  return nombreFichero;
+    std::string nombre;
+    std::map<std::string, BITMAP*>::iterator it;
+
+    it = bitmaps.begin ();
+    while ( (it!=bitmaps.end()) && nombre.empty() )
+    {
+        // Para 'debug' mostramos el proceso de búsqueda.
+        // cout << it->first << " => " << it->second << endl;
+        if (it->second == puntero)
+        {
+            nombre = it->first;
+        }
+        it++;
+    }
+    // Para 'debug' mostramos los valores finales.
+    // cout << "Puntero de bitmap a buscar: " << puntero << endl \
+    //      << "Nombre asociado en 'map': " << nombre << endl;
+    return nombre;
 };
 
-/**
- * \brief   Añade la GUI de la clase a la GUI padre.
- * \details Crea una GUI para esta clase y la añade a la GUI padre pasada como parámetro.
- * \param   gui_padre   Referencia a un conjunto de estructuras 'DIALOG allegro' donde
- *                      incluir la nueva GUI asociada a la clase.
- */
-void  Almacen::addGUI (vector<DIALOG> &gui_padre)
+std::vector<DIALOG>& Almacen::getDIALOG ()
 {
-  // Se crea una GUI para esta clase.
-  gui = new AlmacenGUI (*this, gui_padre);
+    return gui->getGUI ();
+};
+
+unsigned int Almacen::getSize () const
+{
+   return (bitmaps.size () + paletas.size () + sonidos.size ());
+};
+
+std::string Almacen::getNombre () const
+{
+    return nombreFichero;
+};
+
+void Almacen::addGUI (std::vector<DIALOG>& gui_padre)
+{
+    // Se crea una GUI para esta clase.
+    gui = new AlmacenGUI (*this, gui_padre);
 };
--- Clases/Almacen.h
+++ Clases/Almacen.h
@@ -1,12 +1,13 @@
-/**
- *  \file     Almacen.h
- *  \brief    Fichero con el código de definición de la clase Almacen.
- *
- *  \author   Juan Manuel Rico
- *  \date     Diciembre 2010
- *  \version  1.00
- *
- */
+///
+/// @file Almacen.h
+/// @brief Fichero con el código de declaración de la clase Almacen.
+/// @author Juan Manuel Rico
+/// @date Diciembre 2010
+/// @version
+///         - 1.0.0 Diciembre 2010
+///         - 1.0.1 Marzo 2015
+///
+
 #ifndef _ALMACEN_H_
 #define _ALMACEN_H_
 
@@ -17,42 +18,131 @@
 #include <map>
 #include <vector>
 
+/// @note Esta clase se usa para editar o visualizar los recursos del almacén.
+///       Lo ideal es que heredara de "EditableObject" y entregara un "Formulario".
 class AlmacenGUI;
 
-using std::map;
-using std::vector;
-using std::string;
-
-/**
- * \class   Almacen
- * \brief   Maneja los recursos (gráficos y sonidos) del juego.
- * \details Esta clase nos debe dar acceso a los distintos recursos que necesita el juego.
- *          Los recursos se obtienen de un fichero de tipo DAT generado por Grabber.
- * \todo    Crear métodos "GetSample" y "SetSample" para manejo de sonidos.
- */
+/// @class Almacen
+/// Maneja los recursos (gráficos y sonidos) del juego.
+///
+/// Esta clase nos debe dar acceso a los distintos recursos que necesita el juego.
+/// Los recursos se obtienen de un fichero de tipo DAT generado por Grabber.
+/// @todo Crear métodos "GetSample" y "SetSample" para manejo de sonidos.
+///
 class Almacen
 {
-  public:
-                      Almacen       (string paramNombreFichero);
-                      Almacen       ();
-                      ~Almacen      ();
-    BITMAP *          getBitmap     (string nombreBitmap);
-    BITMAP *          getBitmap     (int indice);
-    RGB *             getPalette    (string nombrePaleta);
-    string            getName       (BITMAP *puntero);
-    string            getNombre     (int indice) const;
-    string            getNombre     () const;
-    unsigned int      getSize       () const;
-    vector<DIALOG> &  getDIALOG     ();
-    void              addGUI        (vector<DIALOG> &dlg_padre);
+public:
+    /// Construye el almacén dando el nombre del fichero DAT a cargar.
+    ///
+    /// Intenta leer el fichero. Si consigue abrirlo genera tablas de los distintos tipos de
+    ///         recursos y sus nombres dados en el fichero.
+    ///         Si no consigue abrirlo, envía un mensaje de error por consola.
+    ///         Se crea también una GUI para poder mostrar todos los recursos.
+    /// @param paramNombreFichero Cadena con el nombre del fichero de tipo DAT.
+    /// @todo Por hacer:
+    ///        - Cuando se produzca el error en la lectura del fichero lazar una excepción para ser capturada.
+    ///        - Eliminar la creación de la GUI en el constructor. Ponerla en un método aparte.
+    ///
+    Almacen (std::string paramNombreFichero);
 
-  private:
-    DATAFILE *                fichero;
-    string                    nombreFichero;
-    map<string, BITMAP *>     bitmaps;
-    map<string, RGB *>        paletas;
-    map<string, SAMPLE *>     sonidos;
-    AlmacenGUI *              gui;
+    /// Constructor por omisión. Crea un almacén vacío y sin GUI.
+    ///
+    Almacen ();
+
+    /// Destructor por omisión.
+    /// @todo Liberar los recursos correctamente.
+    ///
+    ~Almacen ();
+
+    /// Devuelve un 'BITMAP allegro' como recurso dando el nombre del bitmap.
+    ///
+    /// Este método se simplifica gracias a la carga previa en la clase 'map'. Se usa como índice
+    /// de la tabla 'map' el nombre obtenido del fichero DAT.
+    /// @param nombreBitmap Nombre identificativo del bitmap dentro del fichero.
+    /// @return  Puntero a la zona de memoria donde se encuentra guardado el BITMAP. 
+    ///
+    BITMAP* getBitmap (std::string nombreBitmap);
+
+    /// Devuelve un 'BITMAP allegro' como recurso dando el índice.
+    /// Este índice es el número identificativo indicado en el fichero
+    /// ".H" generado por Grabber.
+    /// @param indice Índice del recurso que se busca.
+    /// @return Puntero a la zona de memoria donde se encuentra guardado el BITMAP.
+    ///         Si el tipo de recurso apuntado por el índice no es un BITMAP devuelve NULL.
+    /// @todo Comprobar que el índice se encuentra dentro del máximo de recursos.
+    ///
+    BITMAP* getBitmap (int indice);
+
+    /// Devuelve un puntero a un array de 256 estructuras RGB (Paleta de colores).
+    ///
+    /// Este procedimiento se simplifica gracias a la carga previa en la clase 'map'. Se usa como
+    /// índice el nombre de la paleta en el fichero DAT.
+    /// @param nombrePaleta Cadena con el nombre de la paleta que queremos obtener.
+    /// @return Puntero a la zona de memoria de las 256 estructuras RGB que conforman la paleta de colores.
+    ///
+    RGB* getPalette (std::string nombrePaleta);
+
+    /// Devuelve el nombre asociado al puntero en 'bitmaps'.
+    ///
+    /// Recorre todo el 'map' y compara los punteros hasta encontrar el dado por parámetro.
+    /// Procedimiento lento. Se utiliza para guardar el BITMAP como cadena, que sólo se usará
+    /// en casos puntuales del juego.
+    /// @param puntero Puntero del bitmap del que queremos obtener su nombre.
+    /// @return Cadena nueva de caracteres con el nombre asociado al puntero. Si no encuentra la
+    ///         asociación con el puntero devolverá una cadena vacía.
+    /// @todo Por hacer:
+    ///     - En lugar de pasar como parámetro el puntero pasar una referencia constante al puntero
+    ///       y así evitar modificaciones accidentales del mismo.
+    ///     - Confirmar que se devuelve una cadena vacía si no se encuentra asociación con el puntero.
+    ///     - Usar una constante DEBUG para el precompilador en lugar de comentarlas.
+    ///
+    std::string getName (BITMAP* puntero);
+
+    /// Devuelve el nombre de un recurso según el índice en el fichero.
+    ///
+    /// El nombre que se devuelve es el que se encuentra asociado al índice en el fichero DAT.
+    /// @param indice Índice del recurso del que obtener el nombre.
+    /// @return Cadena con el nombre del recurso, según el obtenido del fichero DAT.
+    ///
+    std::string getNombre (int indice) const;
+
+    /// Obtiene el nombre del fichero desde donde se han cargado los recursos del almacén.
+    /// @return  Nombre del fichero DAT desde donde se obtuvieron los recursos.
+    ///
+    std::string getNombre () const;
+
+    /// Obtiene el número total de recursos en el almacén.
+    ///
+    /// Se obtienen los recursos guardados desde el fichero DAT al crear el almacén.
+    /// Pueden existir recursos de distinto tipo a los guardados en el fichero y estos no se
+    /// contarán.
+    /// @return El número de recursos que se encuentran en el almacén.
+    /// @todo Añadir una variable en la clase que nos diga el número de recursos en el fichero que
+    ///       existen, pero que no se pudieron clasificar.
+    ///
+    unsigned int getSize () const;
+
+    /// Pasa de un dialog estático a otro dinámico.
+    /// @return Referencia a un conjunto de diálogos.
+    /// @todo Confirmar que este método es usado y es necesario.
+    ///
+    std::vector<DIALOG>& getDIALOG ();
+
+    /// Añade la GUI de la clase a la GUI padre.
+    ///
+    /// Crea una GUI para esta clase y la añade a la GUI padre pasada como parámetro.
+    /// @param dlg_padre Referencia a un conjunto de estructuras 'DIALOG allegro' donde
+    ///                  incluir la nueva GUI asociada a la clase.
+    ///
+    void addGUI (std::vector<DIALOG>& dlg_padre);
+
+private:
+    DATAFILE* fichero;                      ///< Puntero de referencia al fichero abierto DAT.
+    std::string nombreFichero;              ///< Nombre del fichero.
+    std::map<std::string, BITMAP*> bitmaps; ///< Lista mapeada de referencias a BITMAP cargados.
+    std::map<std::string, RGB*> paletas;    ///< Lista mapeada de referencias a paletas cargadas.
+    std::map<std::string, SAMPLE*> sonidos; ///< Lista mapeada de referencias a sonidos cargadas.
+    AlmacenGUI* gui;                        ///< Puntero de referencia a la GUI de almacén.
 };
 
-#endif // _ALMACEN_H_
+#endif //_ALMACEN_H_
