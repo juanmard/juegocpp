e368ff76e0d554d36842317f08f7b7f678c4733c
Juanma Rico <juanmard@gmail.com>
2019-12-17 10:50:39 +0100

    Frame: Limpieza de código.

--- Clases/Frame.cpp
+++ Clases/Frame.cpp
@@ -1,33 +1,37 @@
+///---------------------------------------------------------
+/// @file       Frame.h
+/// @author     Juan Manuel Rico
+/// @date       Noviembre 2015
+/// @version    1.1.0
 ///
-/// @file Frame.cpp
-/// @brief Fichero de implementación de la clase "Frame".
-/// @author Juan Manuel Rico
-/// @date Noviembre 2015
-/// @version 1.0.0
+/// @brief      Gráfico básico de una animación.
+///---------------------------------------------------------
 ///
 
 #include "Frame.h"
 #include <sstream>
 
-Frame::Frame ()
-{
-};
+namespace fwg {
+    
+    Frame::Frame ()
+    {
+    }
 
-Frame::~Frame ()
-{
-};
+    Frame::~Frame ()
+    {
+    }
 
-std::string Frame::print () const
-{
-  std::ostringstream cadena;
+    std::string Frame::print () const
+    {
+      std::ostringstream cadena;
 
-  // El puntero 'bmp' hay que transformarlo en el nombre asociado en almacén.
-  cadena << bmp << ", Pos <" << cx << "," << cy << "> " << "Ticks: " << ticks;
-  return (cadena.str());
-};
+      // El puntero 'bmp' hay que transformarlo en el nombre asociado en almacén.
+      cadena << bmp << ", Pos <" << cx << "," << cy << "> " << "Ticks: " << ticks;
+      return (cadena.str());
+    }
 
-BITMAP* Frame::getBitmap () const
-{
-    return bmp;
-};
-
+    BITMAP* Frame::getBitmap () const
+    {
+        return bmp;
+    }
+}
--- Clases/Frame.h
+++ Clases/Frame.h
@@ -1,9 +1,11 @@
+///---------------------------------------------------------
+/// @file       Frame.h
+/// @author     Juan Manuel Rico
+/// @date       Noviembre 2015
+/// @version    1.1.0
 ///
-/// @file Frame.h
-/// @brief Fichero de definición de la clase "Frame".
-/// @author Juan Manuel Rico
-/// @date Noviembre 2015
-/// @version 1.0.0
+/// @brief      Gráfico básico de una animación.
+///---------------------------------------------------------
 ///
 
 #ifndef _FRAME_H_
@@ -13,37 +15,47 @@
 #include "Mask.h"
 #include <string>
 
-/// Cuadro básico (Frame) de la animación.
-///
-/// @todo Cambiar el BITMAP que es un tipo de Allegro, por un tipo Bitmap propio.
-/// @todo Generar los procedimientos necesarios para dejar las propiedades privadas y no públicas.
-/// @todo Agrupar cx y cy en un único vector.
-///
-class Frame
-{
-public:
-  BITMAP* bmp;  ///< Imagen que debe mostrar el cuadro.
-  fwg::Mask* mask;   ///< Máscara de colisión del cuadro.
-  int cx;       ///< Coordenada x de la posición relativa del cuadro.
-  int cy;       ///< Coordenada y de la posición relativa del cuadro.
-  int ticks;    ///< Tiempo (en ciclos de juego) que debe permanecer el cuadro en pantalla.
+namespace fwg {
 
-public:
-    /// Constructor básico.
+    /// Cuadro básico (Frame) de la animación.
     ///
-    Frame ();
-
-    /// Destructor básico.
+    /// @todo Por hacer en esta clase:
+    ///         - Cambiar el BITMAP que es un tipo de Allegro, por un tipo Bitmap propio.
+    ///         - Generar los procedimientos necesarios para dejar las propiedades privadas y no públicas.
+    ///         - Agrupar cx y cy en un único vector.
+    ///         - Heredar de 'PrintableObject' para el método 'print'.
     ///
-    ~Frame ();
+    class Frame
+    {
+        public:
+            BITMAP* bmp;       ///< Imagen que debe mostrar el cuadro.
+            fwg::Mask* mask;   ///< Máscara de colisión del cuadro.
+            int cx;            ///< Coordenada x de la posición relativa del cuadro.
+            int cy;            ///< Coordenada y de la posición relativa del cuadro.
+            int ticks;         ///< Tiempo (en ciclos de juego) que debe permanecer el cuadro en pantalla.
 
-    /// Entrega una cadena con la estructura de datos del cuadro.
-    /// @return Cadena de caracteres con la estructura del cuadro.
-    std::string print () const;
+        public:
+            /// Constructor básico.
+            ///
+            Frame ();
 
-    /// Entrega un puntero a la imagen del "frame".
-    /// @return Puntero al BITMAP de Allegro con la imagen del "frame".
-    BITMAP* getBitmap () const;
-};
+            /// Destructor básico.
+            ///
+            ~Frame ();
+
+            /// Entrega una cadena con la estructura de datos del cuadro.
+            /// 
+            /// @return Cadena de caracteres con la estructura del cuadro.
+            /// 
+            std::string print () const;
+
+            /// Entrega un puntero a la imagen del "frame".
+            /// 
+            /// @return Puntero al BITMAP de Allegro con la imagen del "frame".
+            /// 
+            BITMAP* getBitmap () const;
+    };
+
+}
 
 #endif
